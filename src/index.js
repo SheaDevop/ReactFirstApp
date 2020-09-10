3ce2b7ee920f9e1c091cd180cd5f9a949770e601
Nacho Dieguez <nacho.dieguez18@gmail.com>
2020-09-10 14:02:49 -0300

    webpackeado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Helloworld from './components/HelloWorld'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Helloworld from './components/HelloWorld.jsx';
 
-ReactDOM.render(<HelloWorld />, document.getElementById(app)) 
+ReactDOM.render(<HelloWorld />, document.getElementById('app')) 
